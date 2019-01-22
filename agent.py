f2823f00427b55de4ec0b940d320a29ffe430230
senri <dv47777@gmail.com>
2019-01-22 01:46:12 +0000

    nothing has change much

--- agent.py
+++ agent.py
@@ -27,22 +27,32 @@ class myAgent:
         self.myPolicy = Linearpolicy(self.policyType,self.num_actions)
     
     def create_encoder(num_samples):
+        """ This methoed creates a convolutional autoencoder trained on the agents current environment
+        specified in self.env for samples (1 sample is 4 frames) specified in num_samples and returns the encoder
+        and the training and validation loss for each epoch."""
+
         self.CAE, self.encoder, self.decoder, self.CAE_loss = ConvAE(num_samples,self.env,self)
         return self.encoder, self.CAE_loss
 
 
     def load_encoder():
-        self.CAE = tf.keras.models.load_model( os.getcwd()+'/CAE_', env ,'_.h5' )
-        self.encoder = tf.keras.models.load_model( os.getcwd()+'/encoder', env ,'_.h5' )
-        self.decoder = tf.keras.models.load_model( os.getcwd()+'/decoder_', env ,'_.h5' )
+        """ This method loads a convolutional autoencoder trained on the environment specified in self.env """
+
+        self.CAE = tf.keras.models.load_model( os.getcwd()+'/CAE_', self.env ,'_.h5' )
+        self.encoder = tf.keras.models.load_model( os.getcwd()+'/encoder', self.env ,'_.h5' )
+        self.decoder = tf.keras.models.load_model( os.getcwd()+'/decoder_', self.env ,'_.h5' )
         return self.encoder
 
     def save_encoder(self.CAE,self.encoder,self.decoder):
-        CAE.save('CAE_', env , '_.h5')
-        encoder.save('encoder_', env , '_.h5')
-        decoder.save('decoder_', env , '_.h5')
+        """ saves the CAE """
+
+        CAE.save('CAE_', self.env , '_.h5')
+        encoder.save('encoder_', self.env , '_.h5')
+        decoder.save('decoder_', self.env , '_.h5')
 
     def getQvalues(state):
+        """ Takes in the state i.e. encoder(observation) and returns an array with length equal to
+        the number of actions, where each element specifies the Q value for a given action. """
 
         Qvalues = np.zeros( (1,self.num_actions) )
         
@@ -52,6 +62,8 @@ class myAgent:
         return Qvalues
 
     def getState(observation):
+        """ Takes in the processed image observations (1 observation is 4 frames) received
+        from collecting the data in the environment and returns encoder(observation). """
 
         states = np.zeros( (observation.shape[0], self.state_size ) )
 
@@ -61,16 +73,24 @@ class myAgent:
         return states
 
     def getAction(observation):
+        """ Input: state is observation
+            Output: if encoder is none outputs random action for random policy,
+            else returns action from epsilon greedy policy."""
 
-        action = None
-        Qvalues = getQvalues(observation)
-        probability = np.random.random_sample()
-
-        if self.epsilon <= probability:
-            action = np.argmax(Qvalues)
+        if self.encoder == None:
+            return np.random.randint(0,high=4)
         else:
-            action = np.random.randint(0,high=4)
-        return action
+            action = None
+            state = self.encoder.predict( observation )
+            Qvalues = getQvalues(state)
+            probability = np.random.random_sample()
+
+            if self.epsilon <= probability:
+                action = np.argmax(Qvalues)
+            else:
+                action = np.random.randint(0,high=4)
+            return action
+        
     
     def imporve_policy():
 

c861859d1bb0c64037bdc0fdfbf6b7ec3d375f0b
senri <dv47777@gmail.com>
2019-01-24 01:14:52 +0000

    Using single method to collect data

--- VAE2.py
+++ VAE2.py
@@ -12,7 +12,18 @@ import torch
 
 def ConvAE(train_samples,envName,agent):
 
-    X,_ = collectObs(train_samples,envName,agent)
+    """ Creats a convolutional autoencoder.
+
+        Input:  train_samples is the number of samples to train on. envName is the name of the
+                environment to get the samples from. agent is an object with method getAction
+                that determines the actions taken in the environment to take samples.
+
+        Returns: Returns encoder, decoder and CAE objects as well as the history of training and
+                 validation losses every epoch."""
+
+
+    X,_,_,_ = collectObs(train_samples,1,envName,agent)
+    X = Img2Frame(X)
 
     height = X.shape[1]
     width = X.shape[2]
@@ -57,69 +68,120 @@ def ConvAE(train_samples,envName,agent):
     return CAE,encoder,decoder,History.history
 
 
-def preprocess(observation):                                                                    # converts rgb to black and white
+def preprocess(observation):    
+
+    """ Converts single RGB image into an image with 1 bit colour of size 84,84
+
+        Input: An image of of shape (210,160,3) with 8 bit colour.
+
+        Returns: An image of shape shape (84,84) with 1 bit colour. """
 
     observation = cv2.cvtColor(cv2.resize(observation,(84,110)), cv2.COLOR_BGR2GRAY)
     observation = observation[26:110,:]                                                         # get rid of first 27 rows of image
     ret, observation = cv2.threshold(observation,1,1,cv2.THRESH_BINARY)
-    return np.reshape(observation,(84,84))
+    return np.reshape(observation,(84,84,1))
     
 
-def collectObs(samples,envName,agent):                                                                       # collects steps amount data, using random actions
+def collectObs(samples,k,envName,agent):            
 
-    steps = samples*4                                                                           # 4 steps is 1 sample
+    """ Collects observations to be used as training data for the convolutional autoencoder.
+
+        Input: samples, the number data points needed in the training set one sample is 4 frames
+               of 1 bit (84,84) images. envName is the environment to collect the observations from.
+               agent is on abject with method getAction which determines how the data will be collected.
+
+        Returns: Obs, 4 dimensional array of shape (steps,84,84,1) of observations 1st dimension is number of steps.
+                 actions, a column vector of shape (steps,1) containing the action made at each step.
+                 rewards, a column vector of shape (steps,1) containing the reward received at each step.
+                 Also returns the number of episodes needed to collect the observaitons.  
+                 Note: Steps is equal to number of frames and samples*4 """
+
+    samples = samples + 1
+    steps = samples*4                                          # 4 steps is 1 sample
     obs = np.zeros( (steps,84,84,1), dtype = 'uint8' )
+    actions = np.zeros( (steps,1) , dtype = 'uint8' )
+    rewards = np.zeros( (steps,1) , dtype = 'uint8' )
     num_episodes = 0
 
     env = gym.make(envName)
     env.reset()
 
-    for n in range 4:
-        observation, reward, done, info = env.step(0)
+    for n in range(4):
+        observation, reward, done, _ = env.step(0)
         obs[n,:,:,:] = preprocess(observation)[np.newaxis]
+        actions[n,0] = 0
+        rewards[n,0] = reward
 
     for i in range(steps-4):
 
-        action = agent.getAction(obs[i:i+4,:,:,:])                                                      # your agent here (this takes random actions)
+        if i%k == 0:
+            action = agent.getAction( Img2Frame(obs[i:i+4,:,:,:]) )                                                     
+
         observation, reward, done, info = env.step(action)
 
-        obs[i+4,:,:,:] = preprocess(observation)[np.newaxis] 
+        obs[i+4,:,:,:] = preprocess(observation)[np.newaxis]
+        actions[i+4,0] = action
+        rewards[i+4,0] = reward 
 
         if done:
             num_episodes = num_episodes + 1
             env.reset()
         
-    return obs.reshape( (-1,84,84,4) ), num_episodes                                                          # returns samples*84*84*4 tensor
+    return obs,actions,rewards,num_episodes                                                  
 
 
-def createPolicy(policyType,num_actions):
+def createPolicy(policyType ,num_actions):
 
-    """ This method initialises linear model objects as the policy based on 
-    the policy type selected between lr = oridinairy least squares linear regressoin,
-    l1 = LASSO, l2 = ridge regression. It also creates a Standard scaler object for
-    standardisation and initialises weights to zero. """
+    """ Initialises linear model objects from OLS linear regression, LASSO and ridge regression.
+        One model is initalised for each action.
 
-    initialise = np.zeros((1,400))
+        Input: policyType, a string from 'lr', 'l1' and 'l2' for OLS, LASSO and ridge respectively. 
+               num_actions specifies how many policy objects to initialise.
+
+        Returns: An array containing 4 linear models one for each action."""
+
+    initialisex = np.zeros((1,400))
+    initialisey = np.zeros((1,1))
+
     policy = []
     if policyType == 'lr':
         for i in range(num_actions):
-            policy.append([lm.LinearRegression(),StandardScaler()])
-            policy[i][0].fit(initialise)
+            policy.append([lm.LinearRegression(),StandardScaler()])     
+            policy[i][0].fit(initialisex,initialisey)                                # Initialises weights to zero
     if policyType == 'l1':
         for i in range(num_actions):
-            policy.append(lm.Lasso(),StandardScaler()])
-            policy[i][0].fit(initialise)
+            policy.append([lm.Lasso(),StandardScaler()])
+            policy[i][0].fit(initialisex,initialisey)
     if policyType == 'l2':
         for i in range(num_actions):
-            policy.append(lm.Ridge(),StandardScaler()])
-            policy[i][0].fit(initialise)
+            policy.append([lm.Ridge(),StandardScaler()])
+            policy[i][0].fit(initialisex,initialisey)
     return policy        
 
-def collectEpisodes(episdoes,envName,agent):                                                                       # collects steps amount data, using random actions
+def Img2Frame(observation):
+    observations = np.moveaxis(observation,3,1).reshape((-1,4,84,84))
+    observation = np.moveaxis(observations,1,3)
+    return observation
+
+
+def collectEpisodes(episdoes,envName,agent):                                                                       
     
+    """ Collects training data used to improve policy.
+
+        Input: episodes, the number of episodes of training data needed. envName is the environment
+               from which to collect the training data. agent is an object which has method getAction
+               to determine how to collect the training data. 
+               
+        Returns: obs, 4 dimensional of size (num_observations,84,84,4) where 1 observation is 4 images
+                 of size (84,84,1).
+                 actions is a column vector of size (steps,1) that contains the actions made at each frame.
+                 rewards is a column vector of size (steps,1) that contains the reward received at each frame. 
+                 steps is the number of frames used to generate the training data. """
+
+
     obs = np.zeros( (1,84,84,1), dtype = 'uint8' )
-    actions = np.zeros( (1) , dtype = 'uint8' )
-    rewards = np.zeros( (1) , dtype = 'uint8' )
+    actions = np.zeros( (1,1) , dtype = 'uint8' )
+    rewards = np.zeros( (1,1) , dtype = 'uint8' )
     num_episodes = 0
     steps = 0
 
@@ -140,6 +202,7 @@ def collectEpisodes(episdoes,envName,agent):                                    
         rewards = np.concatenate((rewards,np.reshape(reward,(1,1))))
 
         if done:
+            print('Episode number: ',num_episodes,'\n')
             num_episodes = num_episodes + 1
             env.reset()
         

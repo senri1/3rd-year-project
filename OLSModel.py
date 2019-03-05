04026d2fdd171a5e5e0cbb624f4c3a31d6c45494
senri <dv47777@gmail.com>
2019-03-05 22:00:14 +0000

    OLS model now implements linear regression using gradient decent using pyTorch

--- OLSModel.py
+++ OLSModel.py
@@ -1,25 +1,37 @@
-import numpy as np 
+import numpy as np
+import torch
 
 class OLS:
 
     def __init__(self):
-        self.weights = None
+        self.dtype = torch.float
+        self.device = torch.device("cpu")
+        self.weights = torch.randn(400, 1, device=self.device, dtype=self.dtype, requires_grad=True)
+
     
     def fit(self,X,Y):
-        inverse = np.linalg.pinv(X)
-        self.weights = np.matmul(inverse,Y)
+        learning_rate = 1e-6
+        X = torch.from_numpy(X)
+        Y = torch.from_numpy(Y)
+        for t in range(500):
+            y_pred = X.mm(self.weights)
+            loss = (y_pred - Y).pow(2).sum()
+            loss.backward()
+            with torch.no_grad():
+                self.weights -= learning_rate * self.weights.grad
+                self.weights.grad.zero_()
 
     def predict(self,X):
-        return np.matmul(X,self.weights)
+        X = torch.from_numpy(X)
+        return X.mm(self.weights)
 
     def getWeights(self):
         return self.weights
 
     def setWeights(self,weight):
-        self.weights = weight
+        self.weights = torch.from_numpy(weight)
     
     def getSquaredError(self,X,Y):
-        prediction = self.predict(X)
-        error = np.abs(prediction-Y)
-        squared_error = np.matmul(np.transpose(error),error)
+        y_pred = X.mm(self.weights)
+        squared_error = (y_pred - Y).pow(2).sum()
         return squared_error
